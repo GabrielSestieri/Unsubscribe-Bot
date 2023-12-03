f075c96ab8e7ac5dd1f51f6d789002e5d0d6ed4e
GabrielSestieri <63177325+GabrielSestieri@users.noreply.github.com>
2023-12-02 23:53:05 -0500

    added get_messages

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from simplegmail import Gmail
+from simplegmail.query import construct_query
 
 gmail = Gmail()
+
+query_params = {
+    "newer_than": (30, "day"),
+    "older_than": (1, "day"),
+}
+
+messages = gmail.get_messages(query=construct_query(query_params))
+
+for message in messages:
+    print(message)
